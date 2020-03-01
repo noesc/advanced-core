89938495104af0b211de76cc34e6010c86085c2f
Noelia Córdoba <noe.scordoba@gmail.com>
2020-03-01 15:49:46 -0300

    Adding migrations

---
+++ EntityFramework/Migrations/20200301184503_MyFirstMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFramework.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(unicode: false, maxLength: 100, nullable: false),
+                    Name = table.Column<string>(unicode: false, maxLength: 150, nullable: false),
+                    Email = table.Column<string>(unicode: false, maxLength: 150, nullable: false),
+                    Phone = table.Column<string>(unicode: false, maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
